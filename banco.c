0b322bd25361055f4bd75aac395d1ec1dbff5857
liv-ea <51425339+liv-ea@users.noreply.github.com>
2019-10-30 03:18:36 -0300

    Update banco.c

--- banco.c
+++ banco.c
@@ -3,63 +3,147 @@
 #include <conio.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
 
-#define MAXCLIENTES 10000
+#define MAXCLIENTES 10000 /* Macro que facilita a alteração do número máximo de clientes */
 
-  struct dados{
+/* Struct que armazena dados dos clientes */
+
+  struct Dados{
 
     char nome[40],
          senha[10],
          conta[5],
-         saldoconta[5];
+         saldo[10];
 
-  } user;
+  };
 
-  char buffer[40],
-       confirmar[10],
-       numconta[6];
+  struct Dados user;
 
-  int clientes[MAXCLIENTES],
-      numdecadastros = 0;
+  /* Struct para armazenar dados de transações */
 
-  FILE *cadastro;
+  struct Transacao{
 
-  char senhadeauto[5] = "34159",
-       pedesenha[5],
-       nome[40];
+    double valor;
 
-  void menu();
-  void cadastrar();
-  void pesquisar();
+    char data[10];
+
+  } transf;
+
+/* Ponteiros para abrir arquivos */
+
+  FILE *cadastro,
+       *tranferencia;
+
+/* Protótipos das funções */
+
+  void menu(int opcao, char pedesenha[7], char senhadeauto[7], char confirmar[10], char nome[40]);
+  void cadastrar(int opcao, char senhadeauto[7], char pedesenha[7], char confirmar[10], char nome[40]);
+  void pesquisar(char pedesenha[7],char confirmar[10], char senhadeauto[7], int opcao, char nome[40]);
   void transacao();
   void ordenar();
-  void listar();
+  void listar(int opcao, char pedesenha[7], char senhadeauto[7], char confirmar[10], char nome[40]);
 
   void pedirsenha();
 
-  int opcao;
+  void gotoxy(int x, int y);
 
 int main() {
 
-  menu();
+
+  /* Configurações de tela */
+
+  /* Troca das cores de fundo e de texto */
+
+  system("color f0");
+
+  /* Declaração de variáveis */
+
+    char confirmar[10],
+         senhadeauto[7],
+         pedesenha[7],
+         nome[40];
+
+    int clientes[MAXCLIENTES],
+        numdecadastros = 0,
+        opcao,
+        y;
+
+  /* Atribuições */
+
+  strcpy(senhadeauto, "34159");
+
+  /* Deixar o prompt em tela cheia (COMANDOS SÓ FUNCIONAM PARA WINDOWS) */
+
+  system("MODE con cols=168 lines=60");
+
+  /* Dando título à barra de títulos para substituir endereço do diretório*/
+
+  system("title Cadastro Bancario");
+
+  /* Chamada do menu para iniciar o programa */
+
+  menu(opcao, pedesenha, senhadeauto, confirmar, nome);
 
   return 0;
+
 }
 
-void menu(){
+void menu(int opcao, char pedesenha[7], char senhadeauto[7], char confirmar[10], char nome[40]){
+
+  int i;
 
   system("cls");
 
-  printf("********************************************************************\n");
-  printf("\t\tBANCO DO UPE\n");
-  printf("\t\tESCOLHA UMA DAS OPCOES ABAIXO\n\n");
-  printf("\t\t1) CADASTRAR\n");
-  printf("\t\t2) PESQUISAR\n");
-  printf("\t\t3) TRANSAÇÃO\n");
-  printf("\t\t4) ORDENAR\n");
-  printf("\t\t5) LISTAR\n");
-  printf("\t\t6) SAIR\n\n");
-  printf("********************************************************************\n");
+  /* Chamada de função para centralizar teto */
+
+  gotoxy(50,14);
+
+  /* for para imprimir parte do menu */
+
+  printf("%c", 201);
+
+  for(i=0 ; i < 70 ; i++){
+
+    printf("%c", 205);
+
+  }
+
+  printf("%c\n", 187);
+  gotoxy(50,15);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,16);
+  printf("%c                            BANCO UPE                                 %c\n", 186, 186);
+  gotoxy(50,17);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,18);
+  printf("%c                  ESCOLHA UMA DAS OPCOES ABAIXO                       %c\n", 186, 186);
+  gotoxy(50,19);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,20);
+  printf("%c                         1) CADASTRAR                                 %c\n", 186, 186);
+  gotoxy(50,21);
+  printf("%c                         2) PESQUISAR                                 %c\n", 186, 186);
+  gotoxy(50,22);
+  printf("%c                       3) TRANSFERENCIA                               %c\n", 186, 186);
+  gotoxy(50,23);
+  printf("%c                          4) ORDENAR                                  %c\n", 186, 186);
+  gotoxy(50,24);
+  printf("%c                          5) LISTAR                                   %c\n", 186, 186);
+  gotoxy(50,25);
+  printf("%c                           6) SAIR                                    %c\n", 186, 186);
+  gotoxy(50,26);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,27);
+  printf("%c", 200);
+
+  for(i=0 ; i < 70 ; i++){
+
+    printf("%c", 205);
+
+  }
+
+    printf("%c\n\n", 188);
 
   scanf("%d", &opcao);
 
@@ -67,13 +151,13 @@ void menu(){
 
     case 1:
 
-      cadastrar();
+       cadastrar(opcao, senhadeauto, pedesenha, confirmar, nome);
 
     break;
 
     case 2:
 
-      pesquisar();
+      pesquisar(pedesenha, confirmar, senhadeauto, opcao, nome);
 
     break;
 
@@ -91,7 +175,7 @@ void menu(){
 
     case 5:
 
-      listar();
+      listar(opcao, pedesenha, senhadeauto, confirmar, nome);
 
     break;
 
@@ -104,23 +188,111 @@ void menu(){
 
 }
 
-void cadastrar(){
+/* Função para centralizar textos na tela */
+
+void gotoxy(int x, int y){
+
+  HANDLE mover;
+  COORD coordenadas;
+
+  mover = GetStdHandle(STD_OUTPUT_HANDLE);
+  coordenadas.X = x;
+  coordenadas.Y = y;
+
+  SetConsoleCursorPosition(mover, coordenadas);
+
+}
+
+void cadastrar(int opcao, char senhadeauto[7], char pedesenha[7], char confirmar[10], char nome[40]){
 
   // Declarações
 
-  int conta;
+  int conta,
+      comp,
+      i;
 
   // Instruções
 
   system("cls");
 
-  printf("********************************************************************\n*\n");
-  printf("*                       BANCO DO UPE                               *\n*\n");
-  printf("*                         CADASTRO                                 *\n*\n");
-  printf("********************************************************************\n\n\n\n");
+  gotoxy(50,1);
 
+  printf("%c", 201);
 
-  printf("\n\nPOR FAVOR, INSIRA O NOME DO CLIENTE:\n");
+  for(i=0 ; i < 70 ; i++){
+
+    printf("%c", 205);
+
+  }
+
+  printf("%c\n", 187);
+
+  gotoxy(50,2);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,3);
+  printf("%c                            BANCO UPE                                 %c\n", 186, 186);
+  gotoxy(50,4);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,5);
+  printf("%c                             CADASTRO                                 %c\n", 186, 186);
+  gotoxy(50,6);
+  printf("%c                                                                      %c\n", 186, 186);
+  gotoxy(50,7);
+  printf("%c", 200);
+
+  for(i=0 ; i < 70 ; i++){
+
+    printf("%c", 205);
+
+  }
+
+  printf("%c", 188);
+
+  gotoxy(50,10);
+
+  puts(senhadeauto);
+
+  printf("          POR FAVOR, INSIRA A SENHA DE AUTORIZACAO DE ACESSO:           \n");
+
+  scanf("%s", pedesenha);
+
+  puts(pedesenha);
+
+  printf("> %d", strcmp(pedesenha, senhadeauto));
+
+  //comp = strcmp(senhadeauto, pedesenha);
+
+  if (strcmp(senhadeauto, pedesenha) != 0){
+
+  //  system("cls");
+
+    printf("********************************************************************\n");
+    printf("*                                                                  *\n");
+    printf("*                           ACESSO NEGADO                          *\n");
+    printf("*                                                                  *\n");
+    printf("********************************************************************\n\n\n");
+
+    system("PAUSE");
+
+    printf("\nDESEJA VOLTAR AO MENU?\n\n\n");
+    printf("1) SIM\n");
+    printf("2) NAO\n\n");
+
+    scanf("%i", &opcao);
+
+    if(opcao == 1){
+
+      menu(opcao, pedesenha, senhadeauto, confirmar, nome);
+
+    } else {
+
+      exit(0);
+
+    }
+
+  } else {
+
+  printf("\n\nINSIRA O NOME DO CLIENTE:\n");
 
   fflush(stdin);
   gets(user.nome);
@@ -157,8 +329,9 @@ void cadastrar(){
 
     if(opcao == 1){
 
-      printf("QUAL EH O SALDO INICIAL DO CLIENTE?\n\n");
-      gets(user.saldoconta);
+      printf("QUAL EH O SALDO INICIAL DO CLIENTE?\n\nFormato xxx.xx\n\n");
+      fflush(stdin);
+      gets(user.saldo);
 
       cadastro = fopen("cadastro.txt", "a");
 
@@ -176,9 +349,12 @@ void cadastrar(){
 
       cadastro = fopen("cadastro.txt", "a");
 
-      fwrite(&user, sizeof(struct dados), 1, cadastro);
+      fwrite(&user, sizeof(struct Dados), 1, cadastro);
 
       fclose(cadastro);
+
+      system("PAUSE");
+      system("cls");
 
       printf("CADASTRO REALIZADO COM SUCESSO\n\n");
       printf("DESEJA REALIZAR MAIS ALGUMA ACAO?\n\n");
@@ -191,14 +367,13 @@ void cadastrar(){
 
       if(opcao == 1){
 
-        menu();
+        menu(opcao, pedesenha, senhadeauto, confirmar, nome);
 
       } else {
 
         exit(0);
 
       }
-
 
     }
 
@@ -214,7 +389,7 @@ void cadastrar(){
 
     if(opcao == 1){
 
-      cadastrar();
+      cadastrar(opcao, senhadeauto, pedesenha, confirmar, nome);
 
     } else {
 
@@ -226,7 +401,9 @@ void cadastrar(){
 
 }
 
-void pesquisar(){
+}
+
+void pesquisar(char pedesenha[7], char confirmar[10], char senhadeauto[7], int opcao, char nome[40]){
 
   // Instruções
 
@@ -256,7 +433,7 @@ void pesquisar(){
 
     if(opcao == 1){
 
-      menu();
+      menu(opcao, pedesenha, senhadeauto, confirmar,nome);
 
     } else {
 
@@ -266,15 +443,16 @@ void pesquisar(){
 
   } else {
 
+    system("cls");
+
     printf("INSIRA O NOME DO CLIENTE\n\n");
+
     fflush(stdin);
     gets(nome);
 
-    cadastro = fopen("cadastro.txt", "a+");
+    cadastro = fopen("cadastro.txt", "r");
 
-    strcpy(buffer, "");
-
-    while (fread(&user, sizeof(struct dados), 1, cadastro)) {
+    while (fread(&user, sizeof(struct Dados), 1, cadastro)) {
 
       if(nome == user.nome){
 
@@ -290,6 +468,24 @@ void pesquisar(){
 
     fclose(cadastro);
 
+    printf("DESEJA REALIZAR MAIS ALGUMA ACAO?\n\n");
+
+    printf("1) SIM\n");
+    printf("2) NAO\n\n");
+
+    fflush(stdin);
+    scanf("%i", &opcao);
+
+    if(opcao == 1){
+
+      menu(opcao, pedesenha, senhadeauto, confirmar,nome);
+
+    } else {
+
+      exit(0);
+
+    }
+
     }
 
   }
@@ -302,15 +498,17 @@ void transacao(){
 
 void ordenar(){
 
+
+
 }
 
-void listar(){
+void listar(int opcao, char pedesenha[7], char senhadeauto[7], char confirmar[10], char nome[40]){
 
   system("cls");
 
   printf("********************************************************************\n\n");
   printf("*                          BANCO DO UPE                            *\n\n");
-  printf("*                        PESQUISAR CONTAS                          *\n\n");
+  printf("*                          LISTAR CONTAS                           *\n\n");
   printf("********************************************************************\n\n\n\n");
   printf("POR FAVOR, INSIRA A SENHA DE AUTORIZACAO DE ACESSO:\n");
 
@@ -332,7 +530,7 @@ void listar(){
 
     if(opcao == 1){
 
-      menu();
+      menu(opcao, pedesenha, senhadeauto, confirmar, nome);
 
     } else {
 
@@ -342,14 +540,17 @@ void listar(){
 
   } else {
 
+    system("cls");
+
     cadastro = fopen("cadastro.txt", "r");
 
-    while (fread(&user, sizeof(struct dados), 1, cadastro)) {
+    while(fread(&user, sizeof(struct Dados), 1, cadastro)) {
 
-      printf("\n\n");
-      printf("NOME: %s\n", user.nome);
+      printf("\n");
+      printf("NOME:%s\n", user.nome);
       printf("SENHA: %s\n", user.senha);
-      printf("NUMERO DA CONTA:\n", user.conta);
+      printf("NUMERO DA CONTA:%s\n", user.conta);
+      printf("SALDO:%s\n", user.saldo);
       printf("-------------------------\n");
 
     }
